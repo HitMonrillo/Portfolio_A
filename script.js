cb32ae209731992db3066f59f40090e381bd6cf3
HitMonrillo <59132883+HitMonrillo@users.noreply.github.com>
2025-09-11 19:04:26 -0400

    First website

---
+++ script.js
@@ -0,0 +1,16 @@
+const now = new Date();
+const hours = now.getHours();
+let greeting;
+
+if (hours < 12) {
+    greeting = "Good morning!";
+} else if (hours < 18) {
+    greeting = "Good afternoon!";
+} else if (hours ==23){
+    greeting = "It's almost midnight!";
+}
+ else {
+    greeting = "Good evening!";
+}
+
+document.getElementById("greeting").innerText = greeting;
